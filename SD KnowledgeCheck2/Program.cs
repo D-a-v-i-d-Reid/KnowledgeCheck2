a061f1a72cb4ee10d318f9434897cab030f24195
David Reid <134351711+D-a-v-i-d-Reid@users.noreply.github.com>
2023-09-16 04:08:39 -0400

    Initial Commit

---
+++ SD KnowledgeCheck2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace SD_KnowledgeCheck2
+{ 
+    class Program
+    {
+        static void Main()
+        { }
+    }
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<MyClass>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var myClass = new MyClass();
+
+    Console.WriteLine("Enter the value for ");
+    myClass.myProperty = Console.ReadLine();
+
+    recordList.Add(myClass);
+}
+}
+// Print out the list of records using Console.WriteLine()
